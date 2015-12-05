e9178b3322ac5a8e16924fbc36dc96729b79206a
Charles Dunbar <ccdunbar@gmail.com>
2015-12-05 11:26:19 -0600

    Now with a working answer
    Previous answer was wrong and bad.

--- Python/problem9.py
+++ Python/problem9.py
@@ -1,14 +1,10 @@
 '''
 Created on Nov 24, 2010
-
 @author: Charles
-
 A Pythagorean triplet is a set of three natural numbers, 
 a  b  c, for which:
-
 a^2 + b^2 = c^2
 For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
-
 There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 Find the product abc
 '''
@@ -27,13 +23,14 @@ def calc_trip(m, n):
 while(1):
     triplet = calc_trip(m,n)
     if(triplet[0] + triplet[1] + triplet[2] < 1000): 
-        print(str(m) + " and " + str(n) + " produce the triplet: " +
+        print(str(n) + " and " + str(m) + " produce the triplet: " +
               str(triplet) + " where the sum does not add up to 1000")
         m+=1
-        n+=1
         continue
-    else:
-        print(triplet)
+    elif (triplet[0] + triplet[1] + triplet[2] == 1000):
+        print "M of {} and N of {} makes {}".format(str(m), str(n), triplet)
+        print "{} + {} + {} = 1000".format(str(triplet[0]), str(triplet[1]), str(triplet[2]))
         break
-
-   
+    else:
+        n+=1
+        m = n+1
