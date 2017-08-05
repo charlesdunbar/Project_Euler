252c165d6d629003f876bb1b28b2bba2d26034b2
Charles Dunbar <charles@puppet.com>
2017-08-05 11:20:26 -0500

    Fix gitignore

---
+++ Go/problem1/problem1.go
@@ -0,0 +1,24 @@
+/*
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+*/
+
+package main
+
+import "fmt"
+
+func sumMultiples(x int) int {
+	retVal := 0
+	for i := 0; i < x; i++ {
+		if i%3 == 0 || i%5 == 0 {
+			retVal += i
+		}
+	}
+	return retVal
+}
+
+func main() {
+	fmt.Println(sumMultiples(1000))
+}
